f2b46b53425be77ab6841c72fa52ca6d2572e059
j19015 <j19015@sangi.jp>
2024-03-21 03:28:16 +0900

    feat fontawsomeを用いてアイコンを追加

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,25 @@
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
+// fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+
 function Navbar() {
   return (
     <nav>
-      <Link to="/">ホーム</Link>
-      <Link to="/createPost">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/createPost">
+        <FontAwesomeIcon icon={faFilePen} />
+        記事投稿
+      </Link>
+      <Link to="/login">
+        <FontAwesomeIcon icon={faArrowRightToBracket} />
+        ログイン
+      </Link>
     </nav>
   );
 }
