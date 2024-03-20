324c222856fd4f5d182e5998d072869892cdb4c7
j19015 <j19015@sangi.jp>
2024-03-21 04:32:50 +0900

    feat ログアウト機能を追加

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/createPost" element={<CreatePost />} />
         <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
-        <Route path="/logout" element={<Logout />} />
+        <Route path="/logout" element={<Logout setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
   );
--- src/components/logout.js
+++ src/components/logout.js
@@ -1,7 +1,24 @@
-function Logout() {
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase.js';
+
+function Logout({ setIsAuth }) {
+  const navigate = useNavigate();
+  const logout = () => {
+    signOut(auth).then(() => {
+      // ログアウト
+      localStorage.removeItem('isAuth');
+      setIsAuth(false);
+      navigate('/login');
+    });
+  };
+
   return (
     <div>
-      <h1>Logout</h1>
+      <p>ログアウトする</p>
+      <button type="button" onClick={logout}>
+        ログアウト
+      </button>
     </div>
   );
 }
