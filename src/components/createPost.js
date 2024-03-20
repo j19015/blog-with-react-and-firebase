c85eeb5dd8656e25a82eefa32cca7da61b6eb062
j19015 <j19015@sangi.jp>
2024-03-21 05:16:26 +0900

    feat 記事投稿ページを作成

--- src/components/createPost.js
+++ src/components/createPost.js
@@ -1,7 +1,22 @@
+import './createPost.css';
+
 function CreatePost() {
   return (
-    <div>
-      <h1>createPost</h1>
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを記入" />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder="投稿内容を記入" />
+        </div>
+        <button type="button" className="postButton">
+          投稿する
+        </button>
+      </div>
     </div>
   );
 }
