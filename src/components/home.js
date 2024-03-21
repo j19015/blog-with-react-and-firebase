42c554755998d0609a5f09038f093dad03f0673f
j19015 <j19015@sangi.jp>
2024-03-22 03:32:11 +0900

    feat homeのレイアウトを追加

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,20 @@
+import './home.css';
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はReactの勉強中です。頑張ってReactエンジニアとして活躍していきたいと思っています。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>takakou</h3>
+          <button type="button">削除</button>
+        </div>
+      </div>
     </div>
   );
 }
