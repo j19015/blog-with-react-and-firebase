d456e1acfeadce66bca970193898ada596899d8f
j19015 <j19015@sangi.jp>
2024-03-23 19:49:01 +0900

    feat 削除機能の実装

--- src/components/home.js
+++ src/components/home.js
@@ -1,30 +1,44 @@
 import { useEffect, useState } from 'react';
 import './home.css';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../firebase';
+/* eslint-disable object-curly-newline */
+import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
+import { auth, db } from '../firebase';
 
 function Home() {
   const [postList, setPostList] = useState([]);
+
+  const getPosts = async () => {
+    const data = await getDocs(collection(db, 'posts'));
+    setPostList(data.docs.map((document) => ({ ...document.data(), id: document.id })));
+  };
+
   useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs(collection(db, 'posts'));
-      console.log(data.docs.map((doc) => ({ ...doc.data() })));
-      setPostList(data.docs.map((doc) => ({ ...doc.data() })));
-    };
     getPosts();
   }, []);
+
+  const handleDelete = async (id) => {
+    if (auth.currentUser.uid !== postList.find((post) => post.id === id).author.id) {
+      return;
+    }
+    await deleteDoc(doc(db, 'posts', id));
+    getPosts();
+  };
 
   return (
     <div className="homePage">
       {postList.map((post) => (
-        <div key={post.title} className="postContents">
+        <div className="postContents" key={post.id}>
           <div className="postHeader">
             <h1>{post.title}</h1>
           </div>
           <div className="postTextContainer">{post.postText}</div>
           <div className="nameAndDeleteButton">
             <h3>{post.author.username}</h3>
-            <button type="button">削除</button>
+            {post.author.id === auth.currentUser?.uid && (
+              <button type="button" onClick={() => handleDelete(post.id)}>
+                削除
+              </button>
+            )}
           </div>
         </div>
       ))}
