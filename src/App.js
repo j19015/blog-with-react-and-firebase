aa4b00e539a03996f2012773e445590e83f4364b
j19015 <j19015@sangi.jp>
2024-03-24 19:49:01 +0900

    feat 自動ログイン機能を追加

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Home from './components/home.js';
+import { useState } from 'react';
 import CreatePost from './components/createPost.js';
+import Home from './components/home.js';
 import Login from './components/login.js';
 import Logout from './components/logout.js';
 import Navbar from './components/navbar.js';
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
-
-  useEffect(() => {
-    if (localStorage.getItem('isAuth')) {
-      setIsAuth(true);
-    }
-  }, []);
+  const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'));
 
   return (
     <Router>
